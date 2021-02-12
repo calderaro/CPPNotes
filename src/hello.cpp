11521ad06fbc75d04ead59d80d6034bda990d88d
Angel Calderaro <calderaro94@gmail.com>
2021-02-11 22:53:52 -0600

    save

---
+++ src/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "hello.hpp"
+
+using namespace std;
+
+void MyFunc()
+{
+    cout << "Hellow world from another .cpp file" << endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "hello.hpp"
+
+int main(int argc, char **argv)
+{
+    MyFunc();
+}
